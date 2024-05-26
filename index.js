81b001efefa06b9334cb88ff3f6f7505abb09a1e
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-05-26 19:51:29 +0100

    make online and members tags update, add events

---
+++ index.js
@@ -0,0 +1,94 @@
+/* Handles dynamically loaded content, mainly the online count and event list */
+document.addEventListener("DOMContentLoaded", function(event) {
+    updateMemberCount();
+    updateEventList();
+});
+
+async function updateMemberCount() {
+    const response = await fetch("https://api.heeler.house/members");
+    const data = await response.json();
+
+    document.getElementById("members_all").innerText = data.all;
+    document.getElementById("members_online").innerText = data.online;
+}
+
+async function updateEventList() {
+    const response = await fetch("https://api.heeler.house/events");
+    const data = await response.json();
+
+    for (let i = 0; i < data.length; i++) {
+        let event = data[i];
+        console.log(event);
+
+        let title = document.createElement("h3");
+        title.classList.add("blueyfont");
+        title.innerText = event.name;
+
+        let time = document.createElement("h6");
+        time.classList.add("blueyfont");
+
+        if (event.ongoing) {
+            time.classList.add("text-success");
+            time.innerText = "Ongoing!";
+        } else {
+            event.start = new Date(event.start);
+            time.innerText = `${event.start.getDate()}${getDateSuffix(event.start.getDate())} of ${getMonthName(event.start.getMonth())} ${event.start.getFullYear()} at ${timeTo12(event.start.getHours(), event.start.getMinutes())}`;
+        }
+
+        let description = document.createElement("p");
+        description.classList.add("mb-4", "overflow-auto");
+        description.innerText = event.description || "No description was provided.";
+
+        let leftSide = document.createElement("div");
+        leftSide.classList.add(event.image == null ? "col-lg-12" : "col-lg-7");
+        leftSide.appendChild(title);
+        leftSide.appendChild(time);
+        leftSide.appendChild(description);
+
+        let color = "secondary";
+        if (i % 2 == 0) color = "primary";
+
+        let container = document.createElement("div");
+        container.classList.add("container", "row", "text-start", "border", "border-"+color+"-subtle", "border-3", "bg-"+color+"-subtle", "rounded-4", "p-3", "text-lg-start", "text-center", "my-2");
+        container.appendChild(leftSide);
+
+        if (event.image != null) {
+            let bannerImage = document.createElement("img");
+            bannerImage.classList.add("col-lg-5", "p-0", "m-0", "align-self-end", "rounded-3", "align-self-center");
+            bannerImage.src = event.image + "?size=512";
+
+            container.appendChild(bannerImage);
+        }
+
+        document.getElementById("ongoingEvents").appendChild(container);
+    }
+
+    data.forEach(event => {
+        
+    });
+}
+
+function getDateSuffix(date) {
+    if ((date.toString().endsWith("1")) && (!date.toString().endsWith("11"))) return "st";
+    if ((date.toString().endsWith("2")) && (!date.toString().endsWith("12"))) return "nd";
+    if ((date.toString().endsWith("2")) && (!date.toString().endsWith("13"))) return "rd";
+    return "th";
+  }
+  
+  function getMonthName(month) {
+    return ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][month];
+  }
+  
+  function fullDigits(number) {
+    if (number.toString().length == 1) return `0${number}`;
+    return `${number}`;
+  }
+  
+  function timeTo12(hours, minutes) {
+    if (hours / 12 > 1) {
+      return `${hours-12}${minutes != 0 ? `:${fullDigits(minutes)}` : ``}pm`;
+    } else {
+      if (hours == 0) hours = 12;
+      return `${hours}${minutes != 0 ? `:${fullDigits(minutes)}` : ``}am`;
+    }
+  }
